c9a83b650c419cc9444df57db38fdff3c55d922a
wchamber01 <williamchambers@gmail.com>
2020-02-16 20:45:33 -0500

    built functioning timer app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import './App.scss';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  // setSeconds(seconds + 1);
+  useEffect(() => {
+    if (isRunning) {
+      const id = window.setInterval(() => {
+        setSeconds(seconds => seconds + 1); //cb function
+      }, 1000);
+      return () => window.clearInterval(id);
+    }
+    return undefined;
+  }, [isRunning]);
+
   return (
     <div className="app">
-      <div className='time-circle'>
-        <div className="time">
-          00:04
-        </div>
+      {/* <button onClick={() => setSeconds(seconds + 1)}>inc seconds</button> */}
+      <div className="time-circle">
+        <div className="time">{seconds}</div>
       </div>
       <div className="buttons">
-        <button className="play-pause">
-          <i className="fa fa-play fa-2x" />
-        </button>
-        <button className="play-pause">
-          <i className="fa fa-pause fa-2x" />
-        </button>
-        <button className="reset">
+        {isRunning ? (
+          <button
+            className="play-pause"
+            onClick={() => {
+              setIsRunning(false);
+            }}
+          >
+            <i className="fa fa-pause fa-2x" />
+          </button>
+        ) : (
+          <button className="play-pause" onClick={() => setIsRunning(true)}>
+            <i className="fa fa-play fa-2x" />
+          </button>
+        )}
+        <button
+          className="reset"
+          onClick={() => {
+            setIsRunning(false);
+            setSeconds(0);
+          }}
+        >
           Reset
         </button>
       </div>
